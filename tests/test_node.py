e7572cd55daa6554ee792c24237e663c93251d70
Yasin Ughur <yasinugur.cs@gmail.com>
2023-02-12 21:28:19 +0300

    :test_tube: Add Node Tests
    Added Node Tests

---
+++ tests/test_node.py
@@ -0,0 +1,72 @@
+import pytest
+from pydatatype.node import Node
+
+
+class TestNode:
+    def test_node_creation(self):
+        new_node = Node(1)
+        assert new_node.value == 1
+        assert new_node.right is None
+        assert new_node.left is None
+
+    def test_node_creation_with_left(self):
+        new_node = Node(1, Node(2))
+        assert new_node.value == 1
+        assert new_node.right is None
+        assert new_node.left.value == 2
+
+    def test_node_creation_with_right(self):
+        new_node = Node(1, None, Node(2))
+        assert new_node.value == 1
+        assert new_node.right.value == 2
+        assert new_node.left is None
+
+    def test_node_creation_with_both(self):
+        new_node = Node(1, Node(2), Node(3))
+        assert new_node.value == 1
+        assert new_node.right.value == 3
+        assert new_node.left.value == 2
+
+    def test_node_creation_with_invalid_left(self):
+        with pytest.raises(TypeError):
+            Node(1, 2)
+
+    def test_node_creation_with_invalid_right(self):
+        with pytest.raises(TypeError):
+            Node(1, None, 2)
+
+    def test_node_creation_with_invalid_both(self):
+        with pytest.raises(TypeError):
+            Node(1, 2, 3)
+
+    def test_node_creation_with_invalid_value(self):
+        with pytest.raises(TypeError):
+            Node('1')
+
+    def test_node_creation_with_invalid_value_and_left(self):
+        with pytest.raises(TypeError):
+            Node('1', 2)
+
+    def test_node_creation_with_invalid_value_and_right(self):
+        with pytest.raises(TypeError):
+            Node('1', None, 2)
+
+    def test_node_creation_with_invalid_value_and_both(self):
+        with pytest.raises(TypeError):
+            Node('1', 2, 3)
+
+    def test_node_get_children(self):
+        new_node = Node(1, Node(2), Node(3))
+        assert new_node.get_children() == [Node(2), Node(3)]
+
+    def test_node_get_children_with_no_children(self):
+        new_node = Node(1)
+        assert new_node.get_children() == []
+
+    def test_node_get_height(self):
+        new_node = Node(1, Node(2), Node(3))
+        assert new_node.get_height() == 2
+
+    def test_node_get_height_with_no_children(self):
+        new_node = Node(1)
+        assert new_node.get_height() == 0
