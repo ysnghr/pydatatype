90f6fe10a3bfbb3ac5ae98c0ee69c508fbb1e3d9
Yasin Ughur <yasinugur.cs@gmail.com>
2023-02-13 15:55:20 +0300

    :rotating_light: Flake8 not used warnings are deleted
    Flake8 not used warnings are deleted

--- pydatatype/trees/__init__.py
+++ pydatatype/trees/__init__.py
@@ -1,7 +1,7 @@
-from .binary_tree import BinaryTree
-from .binary_search_tree import BinarySearchTree
-from .avl_tree import AVLTree
-from .red_black_tree import RedBlackTree
-from .b_tree import BTree
-from .splay_tree import SplayTree
-from .general_tree import GeneralTree
+from .binary_tree import BinaryTree  # noqa: F401
+from .binary_search_tree import BinarySearchTree  # noqa: F401
+from .avl_tree import AVLTree  # noqa: F401
+from .red_black_tree import RedBlackTree  # noqa: F401
+from .b_tree import BTree  # noqa: F401
+from .splay_tree import SplayTree  # noqa: F401
+from .general_tree import GeneralTree  # noqa: F401
