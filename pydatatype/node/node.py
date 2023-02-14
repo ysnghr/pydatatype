a2ca1d1d4d3e5407ee00ec2dcd5ecbcefab54f91
Yasin Ughur <yasinugur.cs@gmail.com>
2023-02-14 21:16:16 +0300

    :art: Node new functions
    Insert and delete function are added

--- pydatatype/node/node.py
+++ pydatatype/node/node.py
@@ -12,11 +12,14 @@ class Node:
         right: Union["Node", None] = None,
     ):
         if value is not None and not isinstance(value, int):
-            raise TypeError("The argument 'value' must be of type 'int'")
+            raise TypeError("The argument 'value' must be of \
+                            type 'int'")
         if left is not None and not isinstance(left, Node):
-            raise TypeError("The argument 'left' must be of type 'Node' or 'None'")
+            raise TypeError("The argument 'left' must be of \
+                            type 'Node' or 'None'")
         if right is not None and not isinstance(right, Node):
-            raise TypeError("The argument 'right' must be of type 'Node' or 'None'")
+            raise TypeError("The argument 'right' must be of \
+                            type 'Node' or 'None'")
         self.value = value
         self.left = left
         self.right = right
@@ -61,3 +64,39 @@ class Node:
             height = height + 1
 
         return height
+
+    def insert_right(self, value):
+        """
+        Inserts a node to the right of the current node
+        """
+        if self.right is None:
+            self.right = Node(value)
+        else:
+            node = Node(value)
+            node.right = self.right
+            self.right = node
+
+    def insert_left(self, value):
+        """
+        Inserts a node to the left of the current node
+        """
+        if self.left is None:
+            self.left = Node(value)
+        else:
+            node = Node(value)
+            node.left = self.left
+            self.left = node
+
+    def delete_left(self):
+        """
+        Deletes the left node of the current node
+        """
+        if self.left is not None:
+            self.left = None
+
+    def delete_right(self):
+        """
+        Deletes the right node of the current node
+        """
+        if self.right is not None:
+            self.right = None
